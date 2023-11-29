a351f76924bc3fd6b144f8ed67b9ea404bba5a32
Carson <carsonjohnbain@gmail.com>
2023-11-28 17:29:38 -0800

    Can now draw in grid

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const container = document.querySelector('#grid-container');
+const grid = document.querySelectorAll('.grid-item');
+const selectButton = document.querySelector('.button');
+const gridItem = document.createElement('div');
+const clearGrid = document.querySelector('.clear');
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
 
-for (let j = 0; j < 16; j++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    row.appendChild(gridItem);
-    }
-    container.appendChild(row);
+selectButton.onclick = () => {
+    const askGrid = prompt("How big would you like the grid?");
+    gridSize(askGrid);
 }
+
+function gridSize(num){
+    for (let i = 0; i < num; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+    
+    for (let j = 0; j < num; j++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        row.appendChild(gridItem);
+        gridItem.onmouseenter = () => {
+            gridItem.style.backgroundColor = 'black'
+        }
+        }
+        container.appendChild(row);
+    }
+}
+
+
+
+// gridSize(askGrid);
